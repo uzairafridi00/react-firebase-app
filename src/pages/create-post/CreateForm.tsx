cccf984cf457dde00589f53575d321deb124c690
Uzair Afridi <uzairafridi00@gmail.com>
2022-10-09 09:31:17 +0500

    Some styling added

--- src/pages/create-post/CreateForm.tsx
+++ src/pages/create-post/CreateForm.tsx
@@ -1,9 +1,10 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import {addDoc, collection} from "firebase/firestore"
-import {db, auth} from "../../config/firebase"
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 interface CreateFormData {
   title: string;
@@ -11,12 +12,12 @@ interface CreateFormData {
 }
 
 export const CreateForm = () => {
-
+  const navigate = useNavigate();
   const [user, loading, error] = useAuthState(auth);
 
   const schema = yup.object().shape({
-    title: yup.string().required("Add a Title"),
-    description: yup.string().required("Add a Description"),
+    title: yup.string().required("Add a Title*"),
+    description: yup.string().required("Add a Description*"),
   });
 
   const {
@@ -27,26 +28,27 @@ export const CreateForm = () => {
     resolver: yupResolver(schema),
   });
 
-  const postsRef = collection(db, "posts")
+  const postsRef = collection(db, "posts");
 
   const onCreatePost = async (data: CreateFormData) => {
-   await addDoc(postsRef, {
-    title: data.title,
-    description: data.description,
-    username: user?.displayName,
-    userId: user?.uid 
-   })
+    await addDoc(postsRef, {
+      title: data.title,
+      description: data.description,
+      username: user?.displayName,
+      userId: user?.uid,
+    });
     // console.log(data);
+    navigate("/");
   };
 
   return (
     <>
       <form action="" onSubmit={handleSubmit(onCreatePost)}>
         <input type="text" placeholder="Title..." {...register("title")} />
-        <p style={{color:"red"}}>{errors.title?.message}</p>
+        <p style={{ color: "red" }}>{errors.title?.message}</p>
         <textarea placeholder="Description..." {...register("description")} />
-        <p style={{color:"red"}}>{errors.description?.message}</p>
-        <input type="submit" />
+        <p style={{ color: "red" }}>{errors.description?.message}</p>
+        <input type="submit" className="submitForm"/>
       </form>
     </>
   );
--- src/pages/create-post/CreatePost.tsx
+++ src/pages/create-post/CreatePost.tsx
@@ -3,7 +3,7 @@ import {CreateForm} from "./CreateForm"
 export const CreatePost = () => {
     return (
         <>
-        <div>
+        <div className="create-post">
             <CreateForm />
         </div>
         </>
